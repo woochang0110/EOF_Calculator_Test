7a60da2416d137d8c2ca0d88c79b737f8b09638b
kenGwon <kgh9959@gmail.com>
2023-11-11 16:29:31 +0900

    계산기 완성

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
+#include "add_sub.h"
+#include "mul_div.h"
+
 int main()
 {
 	std::cout << "Hello World" << std::endl;
+
+	int a = 20, b = 10;
+
+	printf("a + b = %d\n", add(a, b));
+	printf("a - b = %d\n", sub(a, b));
+	printf("a * b = %d\n", mul(a, b));
+	printf("a / b = %d\n", div_yg(a, b));
 
 	return 0;
 }
---
+++ Calculator/mul_div.h
@@ -0,0 +1,4 @@
+#pragma once
+
+int mul(const int first_operand, const int second_operand);
+int div_yg(const int first_operand, const int second_operand);
