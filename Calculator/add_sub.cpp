ef9f38647079f055ed36e5ebed5aeea3f8065b83
DohyeonP <ehgus922@gmail.com>
2023-11-11 15:56:24 +0900

    덧셈 뺄셈 추가

---
+++ Calculator/add_sub.cpp
@@ -0,0 +1,8 @@
+int add(int first_operand, int second_operand)
+{
+	return first_operand + second_operand;
+}
+int sub(int first_operand, int second_operand)
+{
+	return first_operand - second_operand;
+}
---
+++ Calculator/add_sub.h
@@ -0,0 +1,4 @@
+#pragma once
+
+int add(int first_operand, int second_operand);
+int sub(int first_operand, int second_operand);
