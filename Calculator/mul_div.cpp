eb207c63ce3a4e019fd7526a9724c1676662f3e0
Yonggeun Park <dydrms3456@naver.com>
2023-11-11 15:55:50 +0900

    곱하기 나누기 구현

---
+++ Calculator/mul_div.cpp
@@ -0,0 +1,11 @@
+
+
+int mul(const int first_operand, const int second_operand)
+{
+	return first_operand * second_operand;
+}
+
+int div(const int first_operand, const int second_operand)
+{
+	return first_operand / second_operand;
+}
